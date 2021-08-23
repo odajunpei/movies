7bc2caddba085988703fde7c1dd11ad4bcc5ddeb
odajunpei <i.penju1022@gmail.com>
2021-08-23 09:48:13 +0000

    [Start]movies

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b137e48c475eb601326579f6d0ecb2355532225364ef361b55b2b6aeae50bc5754256a65b0c5714f1e5b2603a1969dc9a08421a7e153b3bed55c8b6c5d008367'
